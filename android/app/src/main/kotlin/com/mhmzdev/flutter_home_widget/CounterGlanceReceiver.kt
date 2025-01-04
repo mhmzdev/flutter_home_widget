468eb9d8b79a972946d4161e60e3b309e51c917e
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2025-01-04 06:55:27 +0500

    [android] interactive home widget

---
+++ android/app/src/main/kotlin/com/mhmzdev/flutter_home_widget/CounterGlanceReceiver.kt
@@ -0,0 +1,7 @@
+package com.mhmzdev.flutter_home_widget
+
+import HomeWidgetGlanceWidgetReceiver
+
+class CounterGlanceWidgetReceiver : HomeWidgetGlanceWidgetReceiver<CounterGlanceWidget>() {
+    override val glanceAppWidget = CounterGlanceWidget()
+}
