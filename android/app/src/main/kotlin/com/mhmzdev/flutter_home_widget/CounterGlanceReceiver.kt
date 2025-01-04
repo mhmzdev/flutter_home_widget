d02da90843cce6d1fda8b1dae6b2084c19e7a25b
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2025-01-04 11:24:42 +0500

    [demo[ wip

--- android/app/src/main/kotlin/com/mhmzdev/flutter_home_widget/CounterGlanceReceiver.kt
+++
@@ -1,7 +0,0 @@
-package com.mhmzdev.flutter_home_widget
-
-import HomeWidgetGlanceWidgetReceiver
-
-class CounterGlanceWidgetReceiver : HomeWidgetGlanceWidgetReceiver<CounterGlanceWidget>() {
-    override val glanceAppWidget = CounterGlanceWidget()
-}
